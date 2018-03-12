b31eae56e0d4763fbf26a2773fa4d8df81f856b2
Yeasteregg <l.d.mattiazzi@gmail.com>
2018-03-13 00:07:45 +0100

    adjusted

--- downloader.js
+++ downloader.js
@@ -4,22 +4,21 @@ const pMapSeries = require('p-map-series')
 const { get } = require('lodash')
 
 const wait = ms => new Promise((resolve, reject) => setTimeout(resolve, ms))
-const downloaded = fs.readFileSync('./downloaded.txt').toString().split('\n')
+const downloaded = fs.readFileSync('./data/downloaded.txt').toString().split('\n')
 
 const start = async () => {
-  const lines = fs.readFileSync('./href.txt').toString().split('\n')
+  const lines = fs.readFileSync('./crawled/results.txt').toString().split('\n')
   const iterator = async (href, idx) => {
     if (downloaded.indexOf(href) !== -1) {
       console.log(`Skipping ${idx}`)
     } else {
       try {
-        const completeHref = `${href.replace('index', 'estrazione')}&ord=4`
         let error = true
         let errors = 0
         let csv
         while (error) {
           errors++
-          csv = await got.get(completeHref)
+          csv = await got.get(href)
           error = get(csv.headers, 'content-disposition') === undefined
           if (errors > 1) {
             console.log(`Trial #${errors} for ${idx}`)
